e50c3cdd6397c9e7f8a91a8ecbdcc700959bb2bd
millytwm1228 <admin@admins-pro-2>
2014-11-18 16:36:35 +0800

    photo

--- com.MusicVideo.test/gen/com/example/com/musicvideo/test/R.java
+++ com.MusicVideo.test/gen/com/example/com/musicvideo/test/R.java
@@ -15,6 +15,10 @@ public final class R {
         public static final int abc_slide_in_top=0x7f040003;
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
+        public static final int left_in=0x7f040006;
+        public static final int left_out=0x7f040007;
+        public static final int right_in=0x7f040008;
+        public static final int right_out=0x7f040009;
     }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
@@ -1078,6 +1082,11 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
+        public static final int swipe_left=0x7f020058;
+        public static final int swipe_right=0x7f020059;
+        public static final int test1=0x7f02005a;
+        public static final int test2=0x7f02005b;
+        public static final int test3=0x7f02005c;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1091,7 +1100,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050040;
+        public static final int action_settings=0x7f050046;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1110,14 +1119,17 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imageview=0x7f050041;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int music=0x7f05003c;
-        public static final int musiclayout=0x7f05003e;
+        public static final int musiclayout=0x7f05003f;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int photo=0x7f05003d;
+        public static final int play=0x7f050042;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1137,13 +1149,16 @@ containing a value of this type.
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
+        public static final int swipe_left=0x7f050043;
+        public static final int swipe_right=0x7f050044;
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
-        public static final int video=0x7f05003d;
-        public static final int videolayout=0x7f05003f;
+        public static final int video=0x7f05003e;
+        public static final int videolayout=0x7f050045;
+        public static final int view_flipper=0x7f050040;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1197,9 +1212,11 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int musiclayout=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
-        public static final int videolayout=0x7f03001b;
+        public static final int left_in=0x7f030019;
+        public static final int musiclayout=0x7f03001a;
+        public static final int photolayout=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int videolayout=0x7f03001d;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1247,7 +1264,10 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int color_baloons=0x7f0a0011;
         public static final int hello_world=0x7f0a000e;
+        public static final int lightning=0x7f0a0010;
+        public static final int natural_wall=0x7f0a0012;
     }
     public static final class style {
         /** 
@@ -1276,6 +1296,8 @@ containing a value of this type.
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f0b008c;
+        public static final int ButtonContainer=0x7f0b008e;
+        public static final int ImageTitle=0x7f0b008d;
         /**  Mimic text appearance in select_dialog_item.xml 
          */
         public static final int TextAppearance_AppCompat_Base_CompactMenu_Dialog=0x7f0b0063;
--- com.MusicVideo.test/src/com/musicvideo/view/MainActivity.java
+++ com.MusicVideo.test/src/com/musicvideo/view/MainActivity.java
@@ -17,39 +17,46 @@ public class MainActivity extends ActionBarActivity {
 
 	private Button musicButton;
 	private Button videoButton;
-	private ArrayList<DataWrapper> mFileList;
+	private ArrayList<DataWrapper> mMusicList;
 	private ArrayList<DataWrapper> mVideoList;
 	private ArrayList<String> mMusicSourceAryList;
 	private ArrayList<String> mVideoSourceAryList;
+	private Button photoButton;
+	private ArrayList<DataWrapper> mPhotoList;
+	private ArrayList<String> mPhotoSourceAryList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		initViews();
-		
+
 		addMusicData();
-		
+
 		addVideoData();
+
+		addPhotoData();
 		
 		setListener();
-		
-		setData(mMusicSourceAryList);
-		
+
+		setMusicData(mMusicSourceAryList);
+
 		setVideoData(mVideoSourceAryList);
+		
+		setPhotoData(mPhotoSourceAryList);
 	}
 
-	private void setData(ArrayList<String> musicSourcePath) {
+	private void setMusicData(ArrayList<String> musicSourcePath) {
 		// mFileList.addAll("http://127.0.0.1:50080/dav/home/Music/A-Lin/%E6%88%91%E5%80%91%E6%9C%83%E6%9B%B4%E5%A5%BD%E7%9A%84/01%20Intro.m4a?session=6278047c683f80b9c8b2bc44f8a6bc43746231c9&login=user");
 		// mFileList.add("http://127.0.0.1:50080/dav/home/Music/Avril%20Lavigne%20(%E8%89%BE%E8%96%87%E5%85%92)_Avril%20Lavigne%20feat_/Let%20Me%20Go/01%20Let%20Me%20Go.m4a?session=6278047c683f80b9c8b2bc44f8a6bc43746231c9&login=user");
 		for (int i = 0; i < musicSourcePath.size(); i++) {
 			DataWrapper wrapper = new DataWrapper();
 			wrapper.filePath = musicSourcePath.get(i);
-			mFileList.add(wrapper);
+			mMusicList.add(wrapper);
 		}
 	}
-	
+
 	private void setVideoData(ArrayList<String> videoSourcePath) {
 		// mFileList.addAll("http://127.0.0.1:50080/dav/home/Music/A-Lin/%E6%88%91%E5%80%91%E6%9C%83%E6%9B%B4%E5%A5%BD%E7%9A%84/01%20Intro.m4a?session=6278047c683f80b9c8b2bc44f8a6bc43746231c9&login=user");
 		// mFileList.add("http://127.0.0.1:50080/dav/home/Music/Avril%20Lavigne%20(%E8%89%BE%E8%96%87%E5%85%92)_Avril%20Lavigne%20feat_/Let%20Me%20Go/01%20Let%20Me%20Go.m4a?session=6278047c683f80b9c8b2bc44f8a6bc43746231c9&login=user");
@@ -59,35 +66,64 @@ public class MainActivity extends ActionBarActivity {
 			mVideoList.add(wrapper);
 		}
 	}
-
+	
+	private void setPhotoData(ArrayList<String> photoSourcePath) {
+		// mFileList.addAll("http://127.0.0.1:50080/dav/home/Music/A-Lin/%E6%88%91%E5%80%91%E6%9C%83%E6%9B%B4%E5%A5%BD%E7%9A%84/01%20Intro.m4a?session=6278047c683f80b9c8b2bc44f8a6bc43746231c9&login=user");
+		// mFileList.add("http://127.0.0.1:50080/dav/home/Music/Avril%20Lavigne%20(%E8%89%BE%E8%96%87%E5%85%92)_Avril%20Lavigne%20feat_/Let%20Me%20Go/01%20Let%20Me%20Go.m4a?session=6278047c683f80b9c8b2bc44f8a6bc43746231c9&login=user");
+		for (int i = 0; i < photoSourcePath.size(); i++) {
+			DataWrapper wrapper = new DataWrapper();
+			wrapper.filePath = photoSourcePath.get(i);
+			mPhotoList.add(wrapper);
+		}
+	}
 
 	private void initViews() {
 		musicButton = (Button) findViewById(R.id.music);
 		videoButton = (Button) findViewById(R.id.video);
-		mFileList = new ArrayList<DataWrapper>();
+		photoButton = (Button) findViewById(R.id.photo);
+		mMusicList = new ArrayList<DataWrapper>();
 		mVideoList = new ArrayList<DataWrapper>();
+		mPhotoList = new ArrayList<DataWrapper>();
 		mMusicSourceAryList = new ArrayList<String>();
 		mVideoSourceAryList = new ArrayList<String>();
-		
+		mPhotoSourceAryList = new ArrayList<String>();
+
 	}
-	
-	private void addMusicData(){
-		mMusicSourceAryList.add("http://192.168.2.152/dav/home/Music/%E4%BF%A1%E6%A8%82%E5%9C%98/%E6%8C%91%E4%BF%A1/01%20%E5%BE%9E%E4%BB%8A%E4%BB%A5%E5%BE%8C.m4a?session=9c32b9b41d76be25e91a4507e55f5deea5ce06e2&login=user");
-		mMusicSourceAryList.add("http://192.168.2.152/dav/home/Music/%E4%BF%A1%E6%A8%82%E5%9C%98/%E6%8C%91%E4%BF%A1/01%20%E5%BE%9E%E4%BB%8A%E4%BB%A5%E5%BE%8C.m4a?session=9c32b9b41d76be25e91a4507e55f5deea5ce06e2&login=user");
-		mMusicSourceAryList.add("http://192.168.2.152/dav/home/Music/%E4%BF%A1%E6%A8%82%E5%9C%98/%E6%8C%91%E4%BF%A1/01%20%E5%BE%9E%E4%BB%8A%E4%BB%A5%E5%BE%8C.m4a?session=9c32b9b41d76be25e91a4507e55f5deea5ce06e2&login=user");
+
+	private void addMusicData() {
+		mMusicSourceAryList
+				.add("http://192.168.2.152/dav/home/Music/%E4%BF%A1%E6%A8%82%E5%9C%98/%E6%8C%91%E4%BF%A1/01%20%E5%BE%9E%E4%BB%8A%E4%BB%A5%E5%BE%8C.m4a?session=9c32b9b41d76be25e91a4507e55f5deea5ce06e2&login=user");
+		mMusicSourceAryList
+				.add("http://192.168.2.152/dav/home/Music/%E4%BF%A1%E6%A8%82%E5%9C%98/%E6%8C%91%E4%BF%A1/01%20%E5%BE%9E%E4%BB%8A%E4%BB%A5%E5%BE%8C.m4a?session=9c32b9b41d76be25e91a4507e55f5deea5ce06e2&login=user");
+		mMusicSourceAryList
+				.add("http://192.168.2.152/dav/home/Music/%E4%BF%A1%E6%A8%82%E5%9C%98/%E6%8C%91%E4%BF%A1/01%20%E5%BE%9E%E4%BB%8A%E4%BB%A5%E5%BE%8C.m4a?session=9c32b9b41d76be25e91a4507e55f5deea5ce06e2&login=user");
 
 	}
 
 	private void addVideoData() {
-		mVideoSourceAryList.add("http://192.168.2.152/dav/home/Videos/a00006.mp4?session=f0f9f8216c6c543e82674da685cbfa5cb0b54af0&login=user");
-		mVideoSourceAryList.add("http://192.168.2.152/dav/home/Videos/a00006.mp4?session=f0f9f8216c6c543e82674da685cbfa5cb0b54af0&login=user");
-		mVideoSourceAryList.add("http://192.168.2.152/dav/home/Videos/a00006.mp4?session=f0f9f8216c6c543e82674da685cbfa5cb0b54af0&login=user");
+		mVideoSourceAryList
+				.add("http://192.168.2.152/dav/home/Videos/a00006.mp4?session=f0f9f8216c6c543e82674da685cbfa5cb0b54af0&login=user");
+		mVideoSourceAryList
+				.add("http://192.168.2.152/dav/home/Videos/a00006.mp4?session=f0f9f8216c6c543e82674da685cbfa5cb0b54af0&login=user");
+		mVideoSourceAryList
+				.add("http://192.168.2.152/dav/home/Videos/a00006.mp4?session=f0f9f8216c6c543e82674da685cbfa5cb0b54af0&login=user");
+
+	}
+	
+	private void addPhotoData() {
+		mPhotoSourceAryList
+				.add("http://192.168.2.152/dav/home/Music/%E4%BF%A1%E6%A8%82%E5%9C%98/%E6%8C%91%E4%BF%A1/01%20%E5%BE%9E%E4%BB%8A%E4%BB%A5%E5%BE%8C.m4a?session=9c32b9b41d76be25e91a4507e55f5deea5ce06e2&login=user");
+		mPhotoSourceAryList
+				.add("http://192.168.2.152/dav/home/Music/%E4%BF%A1%E6%A8%82%E5%9C%98/%E6%8C%91%E4%BF%A1/01%20%E5%BE%9E%E4%BB%8A%E4%BB%A5%E5%BE%8C.m4a?session=9c32b9b41d76be25e91a4507e55f5deea5ce06e2&login=user");
+		mPhotoSourceAryList
+				.add("http://192.168.2.152/dav/home/Music/%E4%BF%A1%E6%A8%82%E5%9C%98/%E6%8C%91%E4%BF%A1/01%20%E5%BE%9E%E4%BB%8A%E4%BB%A5%E5%BE%8C.m4a?session=9c32b9b41d76be25e91a4507e55f5deea5ce06e2&login=user");
 
 	}
 
 	private void setListener() {
 		musicButton.setOnClickListener(musicOnClickListener);
 		videoButton.setOnClickListener(videoOnClickListener);
+		photoButton.setOnClickListener(photoOnClickListener);
 	}
 
 	OnClickListener musicOnClickListener = new OnClickListener() {
@@ -98,8 +134,8 @@ public class MainActivity extends ActionBarActivity {
 			Intent intent = new Intent();
 			Bundle bundle = new Bundle();
 			bundle.putParcelableArrayList("musicurl",
-					(ArrayList<? extends Parcelable>) mFileList);
-			
+					(ArrayList<? extends Parcelable>) mMusicList);
+
 			intent.putExtras(bundle);
 			intent.setClass(getApplicationContext(), MusicActivity.class);
 			startActivity(intent);
@@ -113,11 +149,27 @@ public class MainActivity extends ActionBarActivity {
 			// TODO
 			Intent intent = new Intent();
 			Bundle bundle = new Bundle();
-			 bundle.putParcelableArrayList("videourl",
-			 (ArrayList<? extends Parcelable>) mVideoList);
+			bundle.putParcelableArrayList("videourl",
+					(ArrayList<? extends Parcelable>) mVideoList);
 
 			intent.putExtras(bundle);
 			intent.setClass(getApplicationContext(), VideoActivity.class);
+			startActivity(intent);
+		}
+	};
+	
+	OnClickListener photoOnClickListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO
+			Intent intent = new Intent();
+			Bundle bundle = new Bundle();
+//			bundle.putParcelableArrayList("photourl",
+//					(ArrayList<? extends Parcelable>) mPhotoList);
+
+			intent.putExtras(bundle);
+			intent.setClass(getApplicationContext(), PhotoActivity.class);
 			startActivity(intent);
 		}
 	};
